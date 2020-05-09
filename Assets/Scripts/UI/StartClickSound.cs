acafb2a2b4cddd4b8331ed58d951fd45a79e0c05
Edgar Espino <edgar.espino@loop.colum.edu>
2020-05-09 02:23:38 -0500

    Debuggin enemy damage  detection

---
+++ Assets/Scripts/UI/StartClickSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Require button component
+[RequireComponent(typeof(Button))]
+
+public class StartClickSound : MonoBehaviour
+{
+    [SerializeField] AudioClip startSound;
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = startSound;
+        source.playOnAwake = false;
+
+        // Add on click
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+    void PlaySound()
+    {
+        source.PlayOneShot(startSound);
+    }
+}
