321b7a660fb0cbf751ff87c6c3f2bae77be80843
Edgar Espino <edgar.espino@loop.colum.edu>
2020-05-10 10:48:51 -0500

    Revert "Updated better name for enemy projectile attack"
    This reverts commit 8bfefaa9359afb8a329365a1c9e03646e6ac61c1.

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,11 +6,14 @@ public class EnemyMovement : MonoBehaviour
 {
     // Enemy speed
     [SerializeField] float enemySpeed;
-    [SerializeField] GameObject engines;
+    [SerializeField] GameObject engine;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * enemySpeed);
+        //GetComponent<ParticleSystem>().Play(engine);
+
+
     }
 }
