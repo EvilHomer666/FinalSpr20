c7a82ca927e953f4f872f246123236b5ec62ade8
Edgar Espino <edgar.espino@loop.colum.edu>
2020-07-08 17:52:40 -0500

    Finally got percentage shield working

---
+++ Assets/Scripts/UI/PercentageCalc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageCalc : MonoBehaviour
+{
+    private Text percetageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        percetageText = GetComponent<Text>();
+    }
+
+public void textUpdate(float value)
+    {
+        //percetageText.text = Mathf.RoundToInt(value * 50) + "%";
+
+        percetageText.text = $"Shields: {Mathf.RoundToInt(value * 25) + "%"}";
+    }
+}
